9bcd906a588b1ab51d195ac7f95eb81d258cb428
joeretro <joeretro@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2002-09-15 16:30:22 +0000

    ClassLibrary now resolves imports using ClassLoaders and automatically checks java.lang.*
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@57 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/java/com/thoughtworks/qdox/model/ClassLibrary.java
+++ src/java/com/thoughtworks/qdox/model/ClassLibrary.java
@@ -30,22 +30,34 @@ public class ClassLibrary {
 	}
 
 	public String findClass(Collection imports, String packageName, String className) {
+		// check if there's in an import ending with the class
+		// (classname = ClassName and imports contains com.blah.ClassName)
 		for (Iterator iterator = imports.iterator(); iterator.hasNext();) {
 				String imprt = (String) iterator.next();
 				if (imprt.endsWith("." + className)){
 						return imprt;
 				}
 		}
+		// check if a class exists in the same package with given name
 		String nameAsIfInSamePackage = packageName + "." + className;
-		if (classes.contains(nameAsIfInSamePackage)){
+		if (contains(nameAsIfInSamePackage)){
 				return nameAsIfInSamePackage;
 		}
-		for (Iterator iterator = classes.iterator(); iterator.hasNext();) {
-			String current = (String) iterator.next();
-			if (current.endsWith("." + className) && imports.contains(current.substring(0, current.lastIndexOf(".")) + ".*")) {
-				return current;
+		// check java.lang.*
+		if (contains("java.lang." + className)) {
+			return "java.lang." + className;
+		}
+		// loop through import statements checking if class exists in .* import
+		for (Iterator iterator = imports.iterator(); iterator.hasNext();) {
+			String imprt = (String)iterator.next();
+			if (imprt.endsWith(".*")) {
+				imprt = imprt.substring(0, imprt.length() - 2);
+				if (contains(imprt + '.' + className)) {
+					return imprt + '.' + className;
+				}
 			}
 		}
+		// bummer...
 		return null;
 	}
 

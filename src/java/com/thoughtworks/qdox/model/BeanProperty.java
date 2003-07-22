1cf86e01d38705cdd9e54184af1ecdc8d7b709f9
rinkrank <rinkrank@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2003-07-22 00:29:58 +0000

    New file
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@180 96a0d7f9-151b-0410-ba26-d816ff112d2f

---
+++ src/java/com/thoughtworks/qdox/model/BeanProperty.java
@@ -0,0 +1,36 @@
+package com.thoughtworks.qdox.model;
+
+/**
+ * 
+ * @author Aslak Helles&oslash;y
+ * @version $Revision$
+ */
+public class BeanProperty {
+    private final String name;
+    private JavaMethod accessor;
+    private JavaMethod mutator;
+
+    public BeanProperty(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public JavaMethod getAccessor() {
+        return accessor;
+    }
+
+    public void setAccessor(JavaMethod accessor) {
+        this.accessor = accessor;
+    }
+
+    public JavaMethod getMutator() {
+        return mutator;
+    }
+
+    public void setMutator(JavaMethod mutator) {
+        this.mutator = mutator;
+    }
+}

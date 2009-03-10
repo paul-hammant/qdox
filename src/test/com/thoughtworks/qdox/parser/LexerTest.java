a3276ea57e67055e0ff64c6fedd9d011f00ec615
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2009-03-10 22:06:27 +0000

    cleanup lexerTest imports
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@581 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/test/com/thoughtworks/qdox/parser/LexerTest.java
+++ src/test/com/thoughtworks/qdox/parser/LexerTest.java
@@ -4,11 +4,8 @@ import junit.framework.TestCase;
 
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.Reader;
 import java.io.StringReader;
 import java.lang.reflect.Field;
-import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
 

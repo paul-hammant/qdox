dabb2483bbc17a237bd0a6f7d6fc799c7f2e3b59
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2010-09-07 09:29:42 +0000

    Prepare JavaParameterTest
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@741 96a0d7f9-151b-0410-ba26-d816ff112d2f

---
+++ src/test/com/thoughtworks/qdox/model/DefaultJavaParameterTest.java
@@ -0,0 +1,31 @@
+package com.thoughtworks.qdox.model;
+
+public class DefaultJavaParameterTest extends JavaParameterTest
+{
+
+    public DefaultJavaParameterTest( String s )
+    {
+        super( s );
+    }
+
+    public Type newType( String typeName )
+    {
+        return new Type(typeName);
+    }
+
+    public JavaParameter newJavaParameter( Type type, String name )
+    {
+        return new JavaParameter(type, name);
+    }
+
+    public JavaMethod newJavaMethod()
+    {
+        return new JavaMethod();
+    }
+
+    public void addParameter( JavaMethod method, JavaParameter parameter )
+    {
+        method.addParameter( parameter );
+    }
+
+}
--- src/test/com/thoughtworks/qdox/model/JavaParameterTest.java
+++ src/test/com/thoughtworks/qdox/model/JavaParameterTest.java
@@ -2,18 +2,26 @@ package com.thoughtworks.qdox.model;
 
 import junit.framework.TestCase;
 
-public class JavaParameterTest extends TestCase {
+public abstract class JavaParameterTest extends TestCase {
 
     public JavaParameterTest(String s) {
         super(s);
     }
+    
+    public abstract Type newType(String typeName);
+    
+    public abstract JavaParameter newJavaParameter(Type type, String name);
+    
+    public abstract JavaMethod newJavaMethod();
+    
+    public abstract void addParameter(JavaMethod method, JavaParameter parameter);
 
     public void testParentMethod() throws Exception {
-        JavaParameter p = new JavaParameter(new Type("x"), "x");
+        JavaParameter p = newJavaParameter(newType("x"), "x");
         assertNull(p.getParentMethod());
 
-        JavaMethod m = new JavaMethod();
-        m.addParameter(p);
+        JavaMethod m = newJavaMethod();
+        addParameter(m, p);
         assertSame(m, p.getParentMethod());
     }
 

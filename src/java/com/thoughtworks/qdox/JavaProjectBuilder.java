27d80581950583c04f898fa1d69d40832383aa7f
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2010-10-09 16:02:11 +0000

    reorder methods, add javadocs
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@793 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/java/com/thoughtworks/qdox/JavaProjectBuilder.java
+++ src/java/com/thoughtworks/qdox/JavaProjectBuilder.java
@@ -8,7 +8,6 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Reader;
-import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -16,67 +15,105 @@ import com.thoughtworks.qdox.directorywalker.DirectoryScanner;
 import com.thoughtworks.qdox.directorywalker.FileVisitor;
 import com.thoughtworks.qdox.directorywalker.SuffixFilter;
 import com.thoughtworks.qdox.library.ClassLibraryBuilder;
+import com.thoughtworks.qdox.library.OrderedClassLibraryBuilder;
 import com.thoughtworks.qdox.library.SortedClassLibraryBuilder;
 import com.thoughtworks.qdox.model.JavaClass;
 import com.thoughtworks.qdox.model.JavaPackage;
 import com.thoughtworks.qdox.model.JavaSource;
 
-
-
+/**
+ * This is the improved version of the JavaDocBuilder. It has the following tasks:
+ * <ul>
+ *   <li>Provide adders for all kind of resources, such as classloaders, java files and source directories</li>
+ *   <li>Provide setters to enable the debug-mode for the Lexer and Parser (which are used when parsing sourcefiles) and the encoding
+ *   <li>Provide getter for retrieving Java Object Models from these libraries, such as JavaSources, JavaClasses and JavaPackages</li>
+ *   <li>Provide a method to search through all the parsed JavaClasses </li>
+ *   <li>Provide store and load methods for the JavaProjectBuilder</li> 
+ * </ul>
+ * 
+ * By default the JavaProjectBuilder will use the {@link} SortedClassLibraryBuilder}, which means it doesn't matter in which order you add the resources,
+ * first all sources and sourcefolders, followed by the classloaders. Another implementation for the ClassLibraryBuilder is the
+ * {@link OrderedClassLibraryBuilder}, which preserves the order in which resources are added. 
+ * By creating a new JavaProjectBuilder with your own ClassLibraryBuilder you can decide which loading strategy should be used.  
+ * 
+ * @author Robert Scholte
+ * @since 2.0
+ */
 public class JavaProjectBuilder
 {
     private final ClassLibraryBuilder classLibraryBuilder;
     
-    public ClassLibraryBuilder setDebugLexer( boolean debugLexer )
-    {
-        return classLibraryBuilder.setDebugLexer( debugLexer );
-    }
-
-    public ClassLibraryBuilder setDebugParser( boolean debugParser )
-    {
-        return classLibraryBuilder.setDebugParser( debugParser );
-    }
-
-    public ClassLibraryBuilder setEncoding( String encoding )
-    {
-        return classLibraryBuilder.setEncoding( encoding );
-    }
-
+    // Constructors
+    
+    /**
+     * Default constructor, which will use the {@link SortedClassLibraryBuilder} implementation
+     */
     public JavaProjectBuilder()
     {        
         this.classLibraryBuilder = new SortedClassLibraryBuilder();
     }
 
+    /**
+     * Custom constructor, so another resource loading strategy can be defined
+     * 
+     * @param classLibraryBuilder custom implementation of {@link ClassLibraryBuilder}
+     */
     public JavaProjectBuilder(ClassLibraryBuilder classLibraryBuilder)
     {        
         this.classLibraryBuilder = classLibraryBuilder;
+    }
+
+    // Lexer and Parser -setters
+    
+    /**
+     * Enable the debugmode for the Lexer
+     * 
+     * @param debugLexer true to enable, false to disable
+     * @return This javaProjectBuilder itself 
+     */
+    public JavaProjectBuilder setDebugLexer( boolean debugLexer )
+    {
+        classLibraryBuilder.setDebugLexer( debugLexer );
+        return this;
+    }
+
+    /**
+     * Enable the debugmode for the Parser
+     * 
+     * @param debugParser true to enable, false to disable
+     * @return This javaProjectBuilder itself
+     */
+    public JavaProjectBuilder setDebugParser( boolean debugParser )
+    {
+        classLibraryBuilder.setDebugParser( debugParser );
+        return this;
+    }
+
+    /**
+     * Sets the encoding when using Files or URL´s to parse.
+     * 
+     * @param encoding
+     * @return
+     */
+    public JavaProjectBuilder setEncoding( String encoding )
+    {
+        classLibraryBuilder.setEncoding( encoding );
+        return this;
     }
 
     public JavaSource addSource(File file) throws IOException, FileNotFoundException {
         return classLibraryBuilder.addSource( file );
     }
     
+    // Resource adders
+    
     public JavaSource addSource(Reader reader) {
         return classLibraryBuilder.addSource( reader );
     }
 
-    public JavaClass getClassByName( String name )
+    public void addSourceFolder( File sourceFolder )
     {
-        return classLibraryBuilder.getClassLibrary().getJavaClass( name );
-    }
-    
-    public JavaSource[] getSources() {
-        return classLibraryBuilder.getClassLibrary().getJavaSources();
-    }
-
-    public JavaClass[] getClasses()
-    {
-        return classLibraryBuilder.getClassLibrary().getJavaClasses();
-    }
-
-    public JavaPackage[] getPackages()
-    {
-        return classLibraryBuilder.getClassLibrary().getJavaPackages();
+        classLibraryBuilder.appendSourceFolder( sourceFolder );
     }
 
     public void addSourceTree( File file )
@@ -87,6 +124,11 @@ public class JavaProjectBuilder
             }
         };
         addSourceTree(file, errorHandler);        
+    }
+
+    public void addClassLoader( ClassLoader classLoader )
+    {
+        classLibraryBuilder.appendClassLoader( classLoader );
     }
 
     public void addSourceTree( File file, final FileVisitor errorHandler )
@@ -104,6 +146,34 @@ public class JavaProjectBuilder
         });
     }
 
+    // Java Object Model -getters
+    
+    public JavaClass getClassByName( String name )
+    {
+        return classLibraryBuilder.getClassLibrary().getJavaClass( name );
+    }
+    
+    public JavaSource[] getSources() {
+        return classLibraryBuilder.getClassLibrary().getJavaSources();
+    }
+
+    public JavaClass[] getClasses()
+    {
+        return classLibraryBuilder.getClassLibrary().getJavaClasses();
+    }
+
+    public JavaPackage getPackageByName( String name )
+    {
+        return classLibraryBuilder.getClassLibrary().getJavaPackage( name );
+    }
+    
+    public JavaPackage[] getPackages()
+    {
+        return classLibraryBuilder.getClassLibrary().getJavaPackages();
+    }
+
+    // Searcher
+    
     public JavaClass[] search( Searcher searcher )
     {
         List results = new LinkedList();
@@ -117,11 +187,12 @@ public class JavaProjectBuilder
         return (JavaClass[]) results.toArray( new JavaClass[0] );
     }
 
-    public void addClassLoader( ClassLoader classLoader )
-    {
-        classLibraryBuilder.appendClassLoader( classLoader );
-    }
-
+    /**
+     * Persist the classLibraryBuilder to a file
+     * 
+     * @param file
+     * @throws IOException
+     */
     public void save( File file ) throws IOException
     {
         FileOutputStream fos = new FileOutputStream(file);
@@ -152,17 +223,4 @@ public class JavaProjectBuilder
         }
         return builder;
     }
-
-    public void addSourceFolder( File sourceFolder )
-    {
-        classLibraryBuilder.appendSourceFolder( sourceFolder );
-    }
-
-    public JavaPackage getPackageByName( String name )
-    {
-        return classLibraryBuilder.getClassLibrary().getJavaPackage( name );
-    }
-    
-    
-
 }

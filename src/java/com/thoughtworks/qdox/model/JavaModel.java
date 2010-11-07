716f3f125eacd849e4d61dd1381be61ebfce998d
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2010-11-07 12:14:57 +0000

    getNamedParameter method added to JavaModel
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@897 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/java/com/thoughtworks/qdox/model/JavaModel.java
+++ src/java/com/thoughtworks/qdox/model/JavaModel.java
@@ -11,6 +11,13 @@ public interface JavaModel
 
     public DocletTag getTagByName( String name );
 
+    /**
+     * Convenience method for <code>getTagByName(String).getNamedParameter(String)</code>
+     * that also checks for null tag.
+     * @since 1.3
+     */
+    public String getNamedParameter(String tagName, String parameterName);
+    
     public String getCodeBlock();
 
     public JavaSource getSource();

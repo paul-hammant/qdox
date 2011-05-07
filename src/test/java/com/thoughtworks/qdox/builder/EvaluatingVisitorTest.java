f744f226efee449ed78dbd73e381b60d52ca1d0d
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2011-05-07 20:09:06 +0000

    add all possible types of EvaluatingVisitor.unaryNumericResultType()
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@1190 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/test/java/com/thoughtworks/qdox/builder/EvaluatingVisitorTest.java
+++ src/test/java/com/thoughtworks/qdox/builder/EvaluatingVisitorTest.java
@@ -10,7 +10,17 @@ public class EvaluatingVisitorTest
     @Test
     public void testUnaryNumericResultType() throws Exception 
     {
-        assertEquals( Integer.class, EvaluatingVisitor.unaryNumericResultType( Integer.valueOf( 0 ) ) );
-        assertEquals( Long.class, EvaluatingVisitor.unaryNumericResultType( Long.valueOf( 0l ) ) );
+        assertEquals( Integer.class, EvaluatingVisitor.unaryNumericResultType( 0 ) );
+        assertEquals( Integer.class, EvaluatingVisitor.unaryNumericResultType( ( byte ) 0  ) );
+        assertEquals( Integer.class, EvaluatingVisitor.unaryNumericResultType( ( short ) 0  ) );
+        assertEquals( Integer.class, EvaluatingVisitor.unaryNumericResultType( ( char ) 0 ) );
+        
+        assertEquals( Long.class, EvaluatingVisitor.unaryNumericResultType( 0L ) );
+        
+        assertEquals( void.class, EvaluatingVisitor.unaryNumericResultType( new Object() ) );
+        assertEquals( void.class, EvaluatingVisitor.unaryNumericResultType( ( double ) 0 ) );
+        assertEquals( void.class, EvaluatingVisitor.unaryNumericResultType( ( float ) 0 ) );
+        assertEquals( void.class, EvaluatingVisitor.unaryNumericResultType( null ) );
     }
+    
 }

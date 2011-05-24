737d95935779c0b2762bf6e6e59432f09848dab1
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2011-05-24 20:12:11 +0000

    Add javadoc
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@1222 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/main/java/com/thoughtworks/qdox/model/JavaConstructor.java
+++ src/main/java/com/thoughtworks/qdox/model/JavaConstructor.java
@@ -1,9 +1,31 @@
 package com.thoughtworks.qdox.model;
 
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
 import java.io.Serializable;
+import java.lang.reflect.Constructor;
 import java.util.List;
 
 /**
+ * Modeled equivalent of {@link Constructor}, providing the most important methods.
+ * Where the original {@link Constructor} is using an Array, this model is using a {@link List}.
  * 
  * @author Robert
  * @since 2.0
@@ -11,23 +33,75 @@ import java.util.List;
 public interface JavaConstructor
     extends JavaModel, JavaAnnotatedElement, JavaGenericDeclaration, JavaMember, Serializable
 {
-
+    // Methods from Constructor
+      
+    /**
+     * Equivalent of {@link Constructor#getParameterTypes()}, where a JavaParameter also contains the original name if available.
+     * 
+     * @return a list of JavaParameters, never <code>null</code>
+     */
     List<JavaParameter> getParameters();
 
+    /**
+     * 
+     * @param name the name of the parameter
+     * @return the {@link JavaParameter} matching the name, otherwise <code>null</code>
+     */
     JavaParameter getParameterByName( String name );
     
+    /**
+     * Equivalent of {@link Constructor#getParameterTypes()}
+     * 
+     * @return a list of JavaParameters, never <code>null</code>
+     */
     List<Type> getParameterTypes();
     
+    /**
+     * Equivalent of {@link Constructor#getExceptionTypes()}
+     * 
+     * @return a list of Exceptions, never <code>null</code>
+     */
     List<Type> getExceptions();
 
-    String getSourceCode();
-    
-    String getCodeBlock();
-
+    /**
+     * Equivalent of {@link Constructor#isVarArgs()} 
+     * 
+     * @return <code>true</code> if the final parameter is a varArg, otherwise <code>false</code>
+     */
     boolean isVarArgs();
 
+    // Extra source methods
+    
+    /**
+     * Retrieve the code between the brackets
+     * 
+     * @return the source code
+     */
+    String getSourceCode();
+    
+    /**
+     * Retrieve the complete codeblock of the constructor, including the sourcecode.
+     * @return the code block
+     */
+    String getCodeBlock();
+
+    // Utility methods
+    
+    /**
+     * Returns <code>true</code> if this constructor matches the parameterTypes, assuming it's a non-varArg constructor.
+     * 
+     * @param parameterTypes
+     * @return <code>true</code> if signature matches, otherwise <code>false</code>
+     */
     boolean signatureMatches( List<Type> parameterTypes );
 
+    /**
+     * Returns <code>true</code> if this constructor matches the parameterTypes and matches the varArg argument.
+     * 
+     * @param parameterTypes
+     * @param varArgs
+     * @return <code>true</code> if signature matches, otherwise <code>false</code>
+     */
     boolean signatureMatches( List<Type> parameterTypes, boolean varArgs );
 
 }

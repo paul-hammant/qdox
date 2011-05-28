d190f83881018448499e28edc4d82bfea5b4d1c1
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2011-05-28 10:48:07 +0000

    Refactored c.t.q.m.e.Constant. Lazy resolving the value, removed Serializable
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@1247 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/main/java/com/thoughtworks/qdox/model/expression/Constant.java
+++ src/main/java/com/thoughtworks/qdox/model/expression/Constant.java
@@ -19,154 +19,208 @@ package com.thoughtworks.qdox.model.expression;
  * under the License.
  */
 
-import java.io.Serializable;
 
+public abstract class Constant
+    implements AnnotationValue
+{
 
-public class Constant implements AnnotationValue, Serializable {
-
-    private final Object value;
     private final String image;
 
-    private Constant( Object value, String image ) {
-        this.value = value;
+    private Constant( String image )
+    {
         this.image = image;
     }
 
-    public static Constant newBooleanLiteral( String value ) 
+    public abstract Object getValue();
+
+    public static Constant newBooleanLiteral( final String value )
     {
-        return new Constant( toBoolean( value ), value );
+        return new Constant( value )
+        {
+
+            @Override
+            public Object getValue()
+            {
+                return toBoolean( value );
+            }
+        };
     }
 
-    public static Constant newCharacterLiteral( String value ) 
+    public static Constant newCharacterLiteral( final String value )
     {
-        return new Constant( toChar( value ), value );
+        return new Constant( value )
+        {
+            @Override
+            public Object getValue()
+            {
+                return toChar( value );
+            }
+        };
     }
 
-    public static Constant newFloatingPointLiteral( String value ) 
+    public static Constant newFloatingPointLiteral( final String value )
     {
-        return new Constant( toFloatingPointLiteral( value ), value );
+        return new Constant( value )
+        {
+            @Override
+            public Object getValue()
+            {
+                return toFloatingPointLiteral( value );
+            }
+        };
     }
 
-    public static Constant newIntegerLiteral( String value ) 
+    public static Constant newIntegerLiteral( final String value )
     {
-        return new Constant( toIntegerLiteral( value ), value );
-    }
-    
-    public static Constant newStringLiteral( String value ) {
-        return new Constant( toString( value ), value );
-    }
-    
-    public Object getValue() {
-        return value;
+        return new Constant( value )
+        {
+            @Override
+            public Object getValue()
+            {
+                return toIntegerLiteral( value );
+            }
+        };
     }
 
-    public String getImage() {
+    public static Constant newStringLiteral( final String value )
+    {
+        return new Constant( value )
+        {
+            @Override
+            public Object getValue()
+            {
+                return toString( value );
+            }
+        };
+    }
+
+    public String getImage()
+    {
         return image;
     }
 
-    public String toString() {
+    public String toString()
+    {
         return image;
     }
 
-    public Object accept( AnnotationVisitor visitor ) {
+    public Object accept( AnnotationVisitor visitor )
+    {
         return visitor.visit( this );
     }
 
-    public String getParameterValue() {
+    public String getParameterValue()
+    {
         return image;
     }
- 
-    protected static String convertString(String str) {
+
+    protected static String convertString( String str )
+    {
         StringBuffer buf = new StringBuffer();
         boolean escaped = false;
         int unicode = 0;
         int value = 0;
         int octal = 0;
         boolean consumed = false;
-        
-        for(int i = 0; i < str.length(); ++ i) {
+
+        for ( int i = 0; i < str.length(); ++i )
+        {
             char ch = str.charAt( i );
-            
-            if(octal > 0) {
-                if( value >= '0' && value <= '7' ) {
+
+            if ( octal > 0 )
+            {
+                if ( value >= '0' && value <= '7' )
+                {
                     value = ( value << 3 ) | Character.digit( ch, 8 );
-                    -- octal;
+                    --octal;
                     consumed = true;
                 }
-                else {
+                else
+                {
                     octal = 0;
                 }
-                
-                if( octal == 0 ) {
-                    buf.append( (char) value );     
+
+                if ( octal == 0 )
+                {
+                    buf.append( (char) value );
                     value = 0;
                 }
             }
-            
-            if(!consumed) {
-                if(unicode > 0) {
+
+            if ( !consumed )
+            {
+                if ( unicode > 0 )
+                {
                     value = ( value << 4 ) | Character.digit( ch, 16 );
-                    
-                    -- unicode;
-            
-                    if(unicode == 0) {
-                        buf.append( (char)value );
+
+                    --unicode;
+
+                    if ( unicode == 0 )
+                    {
+                        buf.append( (char) value );
                         value = 0;
                     }
                 }
-                else if(escaped) {
-                    if(ch == 'u' || ch == 'U') {
+                else if ( escaped )
+                {
+                    if ( ch == 'u' || ch == 'U' )
+                    {
                         unicode = 4;
                     }
-                    else if(ch >= '0' && ch <= '7') {
-                        octal = (ch > '3') ? 1 : 2;
+                    else if ( ch >= '0' && ch <= '7' )
+                    {
+                        octal = ( ch > '3' ) ? 1 : 2;
                         value = Character.digit( ch, 8 );
                     }
-                    else {
-                        switch( ch ) {
+                    else
+                    {
+                        switch ( ch )
+                        {
                             case 'b':
-                                buf.append('\b');
+                                buf.append( '\b' );
                                 break;
-                                
+
                             case 'f':
-                                buf.append('\f');
+                                buf.append( '\f' );
                                 break;
-                                
+
                             case 'n':
-                                buf.append('\n');
+                                buf.append( '\n' );
                                 break;
-                                
+
                             case 'r':
-                                buf.append('\r');
+                                buf.append( '\r' );
                                 break;
-                                
+
                             case 't':
-                                buf.append('\t');
+                                buf.append( '\t' );
                                 break;
-                                
+
                             case '\'':
-                                buf.append('\'');
+                                buf.append( '\'' );
                                 break;
-        
+
                             case '\"':
-                                buf.append('\"');
+                                buf.append( '\"' );
                                 break;
-        
+
                             case '\\':
-                                buf.append('\\');
+                                buf.append( '\\' );
                                 break;
-                                
+
                             default:
-                                //yyerror( "Illegal escape character '" + ch + "'" );
+                                // yyerror( "Illegal escape character '" + ch + "'" );
                         }
                     }
-                    
+
                     escaped = false;
                 }
-                else if(ch == '\\') {
+                else if ( ch == '\\' )
+                {
                     escaped = true;
                 }
-                else {
+                else
+                {
                     buf.append( ch );
                 }
             }
@@ -175,115 +229,132 @@ public class Constant implements AnnotationValue, Serializable {
         return buf.toString();
     }
 
-    protected static  Boolean toBoolean(String str) {
+    protected static Boolean toBoolean( String str )
+    {
         str = str.trim();
 
         return Boolean.valueOf( str );
     }
 
-    protected static Byte toByte(String str) {
-       str = str.trim().replaceAll("_", "");
-       
-       return Byte.valueOf(str.substring(2), 2);
+    protected static Byte toByte( String str )
+    {
+        str = str.trim().replaceAll( "_", "" );
+
+        return Byte.valueOf( str.substring( 2 ), 2 );
     }
 
-    protected static Number toIntegerLiteral(String str) {
-        str = str.trim().replaceAll("_", "");
-        
+    protected static Number toIntegerLiteral( String str )
+    {
+        str = str.trim().replaceAll( "_", "" );
+
         Number result;
-        int radix = 10; //default
+        int radix = 10; // default
         int offset = 0;
-        
-        if(str.startsWith("0x") || str.startsWith( "0X" ) ) {
+
+        if ( str.startsWith( "0x" ) || str.startsWith( "0X" ) )
+        {
             radix = 16;
             offset = 2;
-            //result = Integer.valueOf( str.substring( 2 ), 16 );
+            // result = Integer.valueOf( str.substring( 2 ), 16 );
         }
-        else if(str.startsWith("0b") || str.startsWith( "0B" ) ) {
+        else if ( str.startsWith( "0b" ) || str.startsWith( "0B" ) )
+        {
             radix = 2;
             offset = 2;
-            //result = Integer.valueOf( str.substring( 2 ), 2 );
+            // result = Integer.valueOf( str.substring( 2 ), 2 );
         }
-        else if(str.length() > 1 && str.startsWith("0") ) {
+        else if ( str.length() > 1 && str.startsWith( "0" ) )
+        {
             radix = 8;
             offset = 1;
-            //result = Integer.valueOf( str.substring( 1 ), 8 );
+            // result = Integer.valueOf( str.substring( 1 ), 8 );
         }
-        else {
-            //result = Integer.valueOf( str );
+        else
+        {
+            // result = Integer.valueOf( str );
         }
-        
-        if( str.matches( ".+[lL]" )) {
+
+        if ( str.matches( ".+[lL]" ) )
+        {
             result = Long.valueOf( str.substring( offset, str.length() - 1 ), radix );
         }
-        else {
+        else
+        {
             result = Integer.valueOf( str.substring( offset ), radix );
         }
-        
+
         return result;
     }
 
-    protected static Number toFloatingPointLiteral(String str) {
-        str = str.trim().replaceAll("_", "");
-        
+    protected static Number toFloatingPointLiteral( String str )
+    {
+        str = str.trim().replaceAll( "_", "" );
+
         Number result;
-         
-        if( str.matches( ".+[dD]" ) ) 
+
+        if ( str.matches( ".+[dD]" ) )
         {
             result = Double.valueOf( str.substring( 0, str.length() - 1 ) );
         }
-        else if ( str.matches( ".+[fF]" ) ) 
+        else if ( str.matches( ".+[fF]" ) )
         {
             result = Float.valueOf( str.substring( 0, str.length() - 1 ) );
-        } 
-        else 
+        }
+        else
         {
             result = Float.valueOf( str );
         }
-        
+
         return result;
     }
 
-    protected static Double toDouble(String str) {
-        str = str.trim().replaceAll("_", "");
+    protected static Double toDouble( String str )
+    {
+        str = str.trim().replaceAll( "_", "" );
 
-        if( !str.endsWith("d") && !str.endsWith("D") ) {
-//            yyerror( "Double literal must end with 'd' or 'D'." );
+        if ( !str.endsWith( "d" ) && !str.endsWith( "D" ) )
+        {
+            // yyerror( "Double literal must end with 'd' or 'D'." );
         }
-        
+
         return Double.valueOf( str.substring( 0, str.length() - 1 ) );
     }
 
     /**
      * Convert a character literal into a character.
      */
-    protected static Character toChar(String str) {
+    protected static Character toChar( String str )
+    {
         str = str.trim();
 
-        if( !str.startsWith("'") && !str.endsWith("'") ) {
-//            yyerror("Character must be single quoted.");
+        if ( !str.startsWith( "'" ) && !str.endsWith( "'" ) )
+        {
+            // yyerror("Character must be single quoted.");
         }
 
         String str2 = convertString( str.substring( 1, str.length() - 1 ) );
-        
-        if( str2.length() != 1) {
-//            yyerror("Only one character allowed in character constants.");
+
+        if ( str2.length() != 1 )
+        {
+            // yyerror("Only one character allowed in character constants.");
         }
-        
+
         return Character.valueOf( str2.charAt( 0 ) );
     }
 
     /**
      * Convert a string literal into a string.
      */
-    protected static String toString(String str) {
+    protected static String toString( String str )
+    {
         str = str.trim();
 
-        if( str.length() < 2 && !str.startsWith("\"") && !str.endsWith("\"") ) {
-//            yyerror("String must be double quoted.");
+        if ( str.length() < 2 && !str.startsWith( "\"" ) && !str.endsWith( "\"" ) )
+        {
+            // yyerror("String must be double quoted.");
         }
 
         String str2 = convertString( str.substring( 1, str.length() - 1 ) );
         return str2;
-    }    
+    }
 }

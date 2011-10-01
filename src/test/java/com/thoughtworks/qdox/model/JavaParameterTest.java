bde500e4275d3ad9ae369d473efc4ee540d66939
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2011-10-01 14:44:34 +0000

    improve JavaParameterTest: equals must ignore name
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@1342 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/test/java/com/thoughtworks/qdox/model/JavaParameterTest.java
+++ src/test/java/com/thoughtworks/qdox/model/JavaParameterTest.java
@@ -4,12 +4,15 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 import junit.framework.TestCase;
 
-public abstract class JavaParameterTest<P extends JavaParameter> extends TestCase {
+public abstract class JavaParameterTest<P extends JavaParameter>
+    extends TestCase
+{
 
-    public JavaParameterTest(String s) {
-        super(s);
+    public JavaParameterTest( String s )
+    {
+        super( s );
     }
-    
+
     // constructors
     protected abstract P newJavaParameter( Type type, String name );
 
@@ -17,38 +20,45 @@ public abstract class JavaParameterTest<P extends JavaParameter> extends TestCas
 
     // setters
     protected abstract void setMethod( P parameter, JavaMethod method );
-    
+
+    protected Type newType( String typeName )
+    {
+        Type result = mock( Type.class );
+        when( result.getFullyQualifiedName() ).thenReturn( typeName );
+        return result;
+    }
+
     public void testHashCode()
     {
         assertTrue( "hashCode should never resolve to 0", newJavaParameter( Type.VOID, "" ).hashCode() != 0 );
-        
+
         P simpleParameter = newJavaParameter( Type.VOID, "", false );
         P varArgParameter = newJavaParameter( Type.VOID, "", true );
-        
-        assertTrue( simpleParameter.hashCode() != varArgParameter.hashCode() );        
+
+        assertTrue( simpleParameter.hashCode() != varArgParameter.hashCode() );
     }
-    
+
     public void testEquals()
     {
         P simpleParameter = newJavaParameter( Type.VOID, "", false );
         P varArgParameter = newJavaParameter( Type.VOID, "", true );
-        
-        assertTrue( !simpleParameter.equals( varArgParameter ) );        
-    }
-    
-    protected Type newType(String typeName) {
-        Type result = mock(Type.class);
-        when( result.getFullyQualifiedName()).thenReturn( typeName );
-        return result;
+        assertTrue( !simpleParameter.equals( varArgParameter ) );
+
+        // name of parameter shouldn't matter
+        P fooParameter = newJavaParameter( Type.VOID, "foo" );
+        P barParameter = newJavaParameter( Type.VOID, "bar" );
+        assertEquals( fooParameter, barParameter );
     }
 
-    public void testParentMethod() throws Exception {
-        P p = newJavaParameter(newType("x"), "x");
-        assertNull(p.getParentMethod());
+    public void testParentMethod()
+        throws Exception
+    {
+        P p = newJavaParameter( newType( "x" ), "x" );
+        assertNull( p.getParentMethod() );
 
-        JavaMethod m = mock(JavaMethod.class);
+        JavaMethod m = mock( JavaMethod.class );
         setMethod( p, m );
-        assertSame(m, p.getParentMethod());
+        assertSame( m, p.getParentMethod() );
     }
 
 }

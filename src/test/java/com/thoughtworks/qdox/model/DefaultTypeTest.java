df926a7c5df07cf42119a57132bfdab4314ae607
rfscholte <rfscholte@96a0d7f9-151b-0410-ba26-d816ff112d2f>
2011-10-17 20:25:36 +0000

    Refactor tests to use assertThat() instead of assertNotEquals()
    git-svn-id: http://svn.codehaus.org/qdox/trunk/qdox@1423 96a0d7f9-151b-0410-ba26-d816ff112d2f

--- src/test/java/com/thoughtworks/qdox/model/DefaultTypeTest.java
+++ src/test/java/com/thoughtworks/qdox/model/DefaultTypeTest.java
@@ -1,5 +1,15 @@
 package com.thoughtworks.qdox.model;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
 import com.thoughtworks.qdox.library.ClassLibrary;
 import com.thoughtworks.qdox.library.ClassLoaderLibrary;
 import com.thoughtworks.qdox.model.impl.DefaultJavaSource;
@@ -7,11 +17,6 @@ import com.thoughtworks.qdox.model.impl.DefaultJavaSource;
 
 public class DefaultTypeTest extends JavaTypeTest<Type>
 {
-
-    public DefaultTypeTest( String s )
-    {
-        super( s );
-    }
 
     public JavaSource newJavaSource( ClassLibrary library )
     {
@@ -33,24 +38,40 @@ public class DefaultTypeTest extends JavaTypeTest<Type>
         return new Type(fullname, dimensions, source);
     }
     
+    @Test
     public void testArrayType() throws Exception {
         Type type = newType("int", 1);
         assertTrue(type.isArray());
     }
 
+    @Test
     public void testComponentType() throws Exception {
         assertNull( newType("int").getComponentType());
         assertEquals("int", newType("int", 1).getComponentType().getFullyQualifiedName());
         assertEquals("long", newType("long", 3).getComponentType().getFullyQualifiedName());
     }
 
-    public void testTypeHasJavaClass() {
+   @Test
+   public void testTypeHasJavaClass() {
         ClassLoaderLibrary library = new ClassLoaderLibrary( null );
         library.addDefaultLoader();
         JavaSource javaSource = newJavaSource(library);
         JavaClass clazz = newType("java.util.HashSet", 0, javaSource);
         JavaClass superClass = clazz.getSuperJavaClass();
         assertEquals("java.util.AbstractSet", superClass.getFullyQualifiedName());
+    }
+    
+    @Test
+    public void testResolving() throws Exception {
+        JavaSource src = mock(JavaSource.class);
+        when(src.getImports()).thenReturn( Collections.singletonList( "foo.*" ) );
+        Type type = Type.createUnresolved("Bar", 0, src);
+        assertEquals(false, type.isResolved());
+        
+        when(src.resolveType( "Bar" )).thenReturn( "foo.Bar" );
+        assertEquals(true, type.isResolved());
+        assertEquals("Bar", type.getValue());
+        assertEquals("foo.Bar", type.getFullyQualifiedName());
     }
 
 }
--- src/test/java/com/thoughtworks/qdox/model/JavaTypeTest.java
+++ src/test/java/com/thoughtworks/qdox/model/JavaTypeTest.java
@@ -1,92 +1,91 @@
 package com.thoughtworks.qdox.model;
 
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.junit.Assert.*;
+import static org.hamcrest.core.IsNot.*;
+import org.junit.Test;
 
-import java.util.Collections;
+public abstract class JavaTypeTest<T extends JavaType>
+{
 
-import junit.framework.TestCase;
+    public abstract T newType( String fullname );
 
-public abstract class JavaTypeTest<T extends JavaType> extends TestCase {
+    public abstract T newType( String fullname, int dimensions );
 
-    public JavaTypeTest(String s) {
-        super(s);
-    }
-    
-    public abstract T newType(String fullname);
-    public abstract T newType(String fullname, int dimensions);
-    public abstract T newType(String fullname, int dimensions, JavaSource source);
-    
-    public void testResolving() throws Exception {
-        JavaSource src = mock(JavaSource.class);
-        when(src.getImports()).thenReturn( Collections.singletonList( "foo.*" ) );
-        Type type = Type.createUnresolved("Bar", 0, src);
-        assertEquals(false, type.isResolved());
-        
-        when(src.resolveType( "Bar" )).thenReturn( "foo.Bar" );
-        assertEquals(true, type.isResolved());
-        assertEquals("Bar", type.getValue());
-        assertEquals("foo.Bar", type.getFullyQualifiedName());
+    @Test
+    public void testToString()
+        throws Exception
+    {
+        assertEquals( "int", newType( "int" ).toString() );
+        assertEquals( "int[]", newType( "int", 1 ).toString() );
+        assertEquals( "long[][][]", newType( "long", 3 ).toString() );
     }
 
-    public void testToString() throws Exception {
-        assertEquals("int", newType("int").toString());
-        assertEquals("int[]", newType("int", 1).toString());
-        assertEquals("long[][][]", newType("long", 3).toString());
-    }
-    
-    public void testFullyQualifiedName() throws Exception {
-        assertEquals("int", newType("int").getFullyQualifiedName());
-        assertEquals("int[]", newType("int", 1).getFullyQualifiedName());
-        assertEquals("long[][][]", newType("long", 3).getFullyQualifiedName());
+    @Test
+    public void testFullyQualifiedName()
+        throws Exception
+    {
+        assertEquals( "int", newType( "int" ).getFullyQualifiedName() );
+        assertEquals( "int[]", newType( "int", 1 ).getFullyQualifiedName() );
+        assertEquals( "long[][][]", newType( "long", 3 ).getFullyQualifiedName() );
     }
 
-    public void testEquals() throws Exception {
-        assertEquals(newType("string"),
-                newType("string"));
-        assertNotEquals(newType("string"),
-                newType("int"));
-        assertNotEquals(newType("long", 1),
-                newType("long"));
-        assertNotEquals(newType("long"),
-                newType("long", 2));
-        assertFalse(newType("int").equals(null));
+    @Test
+    public void testEquals()
+        throws Exception
+    {
+        assertEquals( newType( "string" ), newType( "string" ) );
+        assertThat( newType( "string" ), not( newType( "int" ) ) );
+        assertThat( newType( "long", 1 ), not( newType( "long" ) ) );
+        assertThat( newType( "long" ), not( newType( "long", 2 ) ) );
+        assertFalse( newType( "int" ).equals( null ) );
     }
 
-    public void testToStringVoid() {
-        assertEquals("void", Type.VOID.toString());
+    @Test
+    public void testToStringVoid()
+    {
+        assertEquals( "void", Type.VOID.toString() );
     }
 
-    public void testToStringBoolean() {
-        assertEquals("boolean", newType("boolean").toString());
-    }
-    
-    public void testToStringInt() {
-        assertEquals("int", newType("int").toString());
+    @Test
+    public void testToStringBoolean()
+    {
+        assertEquals( "boolean", newType( "boolean" ).toString() );
     }
 
-    public void testToStringLong() {
-        assertEquals("long", newType("long").toString());
+    @Test
+    public void testToStringInt()
+    {
+        assertEquals( "int", newType( "int" ).toString() );
     }
 
-    public void testToStringFloat() {
-        assertEquals("float", newType("float").toString());
+    @Test
+    public void testToStringLong()
+    {
+        assertEquals( "long", newType( "long" ).toString() );
     }
 
-    public void testToStringDouble() {
-        assertEquals("double", newType("double").toString());
-    }
-    
-    public void testToStringChar() {
-        assertEquals("char", newType("char").toString());
+    @Test
+    public void testToStringFloat()
+    {
+        assertEquals( "float", newType( "float" ).toString() );
     }
 
-    public void testToStringByte() {
-        assertEquals("byte", newType("byte").toString());
+    @Test
+    public void testToStringDouble()
+    {
+        assertEquals( "double", newType( "double" ).toString() );
     }
-    
-    private void assertNotEquals(Object o1, Object o2) {
-        assertTrue(o2.toString() + " should not equal " + o1.toString(),
-                !o2.equals(o1));
+
+    @Test
+    public void testToStringChar()
+    {
+        assertEquals( "char", newType( "char" ).toString() );
     }
+
+    @Test
+    public void testToStringByte()
+    {
+        assertEquals( "byte", newType( "byte" ).toString() );
+    }
+
 }
